cb299a55cbc40436779f7b95c129c2b73ebdaa6e
Ratul Hasan Rabby <hasanratul647@gmail.com>
2025-07-21 01:08:20 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+    
+ 
+    
+//.........................header......................................
+//.........................header......................................
+      $('header').ripples({
+      dropRadius: 11,
+      perturbance: 0.01,
+       
+    });
+    
+    
+    $(".h-one").addClass("animated fadeInLeft"); 
+    $(".p-one").addClass("animated fadeInRight"); 
+    $(".clicknow").addClass("animated flip"); 
+        
+    
+    $(".menu-button").click(function(){
+        $("nav").slideToggle(1000);
+        
+        
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
